c0a4418b0e41dbb8be3b14b2404489ef760e3b09
ale97 <presaguirre@gmail.com>
2017-10-12 10:46:49 -0600

    add change in material layout

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,7 +1,17 @@
-module.exports={
+module.exports = {
 
-    index: function(req, res, next){
+    index: function(req, res, next) {
         res.render('index');
+    },
+    getMtsAluminio: function(req, res, next) {
+        res.render('aluminio/index');
+    },
+    getMtsVidrio: function(req, res, next) {
+        res.render('vidrio/index');
+    },
+    getMtsAccesorios: function(req, res, next) {
+        res.render('accesorio/index');
     }
+
 
 }
--- routes/routes.js
+++ routes/routes.js
@@ -15,17 +15,30 @@ router.post('/user/signin', passport.authenticate('local', {
 
 }));
 router.get('/user/logout', controllers.UserController.logout);
-router.get('/user/profile',authMiddleware.isLogged, controllers.UserController.profile);
-router.get('/user/signup',authMiddleware.isLogged, controllers.UserController.getSignUp);
-router.post('/user/signup',authMiddleware.isLogged, controllers.UserController.postSignUp);
+router.get('/user/profile', authMiddleware.isLogged, controllers.UserController.profile);
+router.get('/user/signup', authMiddleware.isLogged, controllers.UserController.getSignUp);
+router.post('/user/signup', authMiddleware.isLogged, controllers.UserController.postSignUp);
 
 // routes images
-router.post('/image',authMiddleware.isLogged, controllers.ImageController.postImage);
-router.get('/image', authMiddleware.isLogged,controllers.ImageController.getImage);
-router.get('/image/proyectos', authMiddleware.isLogged,controllers.ImageController.getImageProyecto);
+router.post('/image', authMiddleware.isLogged, controllers.ImageController.postImage);
+router.get('/image', authMiddleware.isLogged, controllers.ImageController.getImage);
+router.get('/image/proyectos', authMiddleware.isLogged, controllers.ImageController.getImageProyecto);
 // routes send email
-
 router.post('/user/send', controllers.UserController.sendEmail);
+
+//materiales
+
+controllers.HomeController.index
+
+router.get('/materiales/aluminio', controllers.HomeController.getMtsAluminio);
+router.get('/materiales/vidrio', controllers.HomeController.getMtsVidrio);
+router.get('/materiales/accesorio', controllers.HomeController.getMtsAccesorios);
+
+
+
+
+
+
 
 
 
