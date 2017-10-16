002aefc752d0d0722de973d5536aaaa35a77b81d
ale97 <presaguirre@gmail.com>
2017-10-16 16:02:28 -0600

    work in front- end

--- public/javascripts/loadImage.js
+++ public/javascripts/loadImage.js
@@ -1,31 +1,19 @@
-// alert('ff');
 $(window).load(function() {
 
     $(function() {
-        //   $('#file-input').change(function(e) {
-        //       addImage(e); 
-        //      });
-
-        //      function addImage(e){
-        //       var file = e.target.files[0],
-        //       imageType = /image.*/;
-
-        //       if (!file.type.match(imageType))
-        //        return;
-
-        //       var reader = new FileReader();
-        //       reader.onload = fileOnload;
-        //       reader.readAsDataURL(file);
-        //      }
         fileOnload();
 
         function fileOnload() {
             var result = 'http://localhost:3000/images/proyectos/slide3.jpg';
             // 1920x1250
 
-            $("#imageSlide").attr("width", "1920");
-            $("#imageSlide").attr("heigth", "1250");
+              $("#imageSlide").attr("width", "1920");
+              $("#imageSlide").attr("heigth", "1250");
             $('#imageSlide').attr("src", result);
         }
     });
 });
+
+// $(document).ready(function() {
+//     $('#imageSlide').css({ 'width':'1920px', 'height':'1250px' });
+// });
