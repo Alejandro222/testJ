5bd6746c239e4c68a7880d165bcfbb618804b3ef
ale97 <presaguirre@gmail.com>
2017-10-25 23:57:37 -0600

    working in contact section

--- public/javascripts/loadImage.js
+++ public/javascripts/loadImage.js
@@ -17,3 +17,16 @@ $(window).load(function() {
 // $(document).ready(function() {
 //     $('#imageSlide').css({ 'width':'1920px', 'height':'1250px' });
 // });
+
+// google maps
+function initMap() {
+      var jarsol = {lat: 10.3758482, lng: -84.3542187};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: jarsol
+      });
+      var marker = new google.maps.Marker({
+        position: jarsol,
+        map: map
+      });
+    }
