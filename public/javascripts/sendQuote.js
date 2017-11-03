0b4d90705776aa3e2424592c63553efe011e8659
ale97 <presaguirre@gmail.com>
2017-11-03 13:53:53 -0600

    add validation in quotes's form

--- public/javascripts/sendQuote.js
+++ public/javascripts/sendQuote.js
@@ -1,76 +1,113 @@
+// $(document).ready(function() {
+//
+
+// });
+
 $(document).ready(function() {
 
-  $("#send-message").click(function(event) {
-    var name = $("#recipient-name").val();
-    var phone = $("#recipient-phone").val();
-    var email = $("#recipient-email").val();
-    var message = $("#message-text").val();
-    var date = new Date();
-    var firstName = name;
-    var item= $("#sel1").find("option:selected").val();
+    var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;    
+    $("#send-message").click(function() {        
+      $(".error").remove();        
+      if ($("#recipient-name").val() == "") {            
+        $("#recipient-name").focus().after("<span class='error'>Ingrese su nombre</span>");            
+        return false;        
+      } else if ($("#recipient-phone").val() == "") {            
+        $("#recipient-phone").focus().after("<span class='error'>Ingrese un número de teléfono</span>");            
+        return false;        
+      } else if ($("#recipient-email").val() == "" || !emailreg.test($("#recipient-email").val())) {            
+        $("#recipient-email").focus().after("<span class='error'>Ingrese un email correcto</span>");            
+        return false;        
+      } else if ($("#message-text").val() == "") {            
+        $("#message-text").focus().after("<span class='error'>Ingrese un mensaje</span>");            
+        return false;        
+      }    
+    else  {
+      // si todo esta bien, se envian los datos
+      $("#send-message").click(function(event) {
+        var name = $("#recipient-name").val();
+        var phone = $("#recipient-phone").val();
+        var email = $("#recipient-email").val();
+        var message = $("#message-text").val();
+        var item = $("#sel1").find("option:selected").val();
 
 
-    if (firstName.indexOf(' ') >= 0) {
-      firstName = name.split(' ').slice(0, -1).join(' ');
+
+
+          var date = new Date();
+          var firstName = name;
+
+
+          if (firstName.indexOf(' ') >= 0) {
+            firstName = name.split(' ').slice(0, -1).join(' ');
+          }
+
+          $.ajax({
+            url: "http://localhost:3000/quote/send",
+            type: "POST",
+            data: {
+              name: name,
+              phone: phone,
+              email: email,
+              message: message,
+              date: date,
+              item: item
+            },
+            cache: false,
+            success: function() {
+              // Success message
+              $("#success").empty();
+              $('#success')
+                .append("<h3>Su cotización a sido enviada.! </h3>");
+
+              //clear all fields
+              $('#cotizacionForm').trigger("reset");
+              $('#QuoteModal').modal('hide');
+              popUp();
+            },
+            error: function() {
+              $('.content-popup')
+                .append($("<h3>").text("Disculpe " + firstName + ",problemas con el envio de datos, intente de de nuevo!"));
+              //clear all fields
+              $('#cotizacionForm').trigger("reset");
+              $('#QuoteModal').modal('hide');
+              popUp();
+            },
+          });
+      });
+      $("#close-modal").click(function(event) {
+        clearInput();
+      });
+
+      function popUp() {
+        $('#popup').fadeIn('slow');
+        $('.popup-overlay').fadeIn('slow');
+        $('.popup-overlay').height($(window).height());
+        return false;
+      };
+      $('#close').click(function() {
+        $('#popup').fadeOut('slow');
+        $('.popup-overlay').fadeOut('slow');
+        return false;
+      });
+
+      function clearInput() {
+        $('#cotizacionForm').trigger("reset");
+      }
+
+
     }
 
-    $.ajax({
-      url: "http://localhost:3000/quote/send",
-      type: "POST",
-      data: {
-        name: name,
-        phone: phone,
-        email: email,
-        message: message,
-        date:date,
-        item:item
-      },
-      cache: false,
-      success: function() {
-        // Success message
-        $("#success").empty();
-        $('#success')
-          .append("<h3>Su cotización a sido enviada.! </h3>");
-
-        //clear all fields
-        $('#cotizacionForm').trigger("reset");
-        $('#QuoteModal').modal('hide');
-        // alert('se envio');
-        popUp();
-      },
-      error: function() {
-        $('.content-popup')
-          .append($("<h3>").text("Disculpe " + firstName + ",problemas con el envio de datos, intente de de nuevo!"));
-        //clear all fields
-        $('#cotizacionForm').trigger("reset");
-        $('#QuoteModal').modal('hide');
-        popUp();
-
-      },
+  });    
+    $("#recipient-name, #message-text, #recipient-phone").keyup(function() {        
+      if ($(this).val() != "") {            
+        $(".error").fadeOut();            
+        return false;        
+      }    
+    });    
+    $("#recipient-email").keyup(function() {        
+      if ($(this).val() != "" && emailreg.test($(this).val())) {            
+        $(".error").fadeOut();            
+        return false;        
+      }    
     });
-  });
-  $("#close-modal").click(function(event) {
-    clearInput();
-  });
-
-  // $('#name').focus(function() {
-  //     $('#success').html('');
-  // });
-  function popUp() {
-    // $('#open').click(function(){
-    $('#popup').fadeIn('slow');
-    $('.popup-overlay').fadeIn('slow');
-    $('.popup-overlay').height($(window).height());
-    return false;
-    // });
-  };
-  $('#close').click(function() {
-    $('#popup').fadeOut('slow');
-    $('.popup-overlay').fadeOut('slow');
-    return false;
-  });
-
-  function clearInput() {
-    $('#cotizacionForm').trigger("reset");
-  }
 });
